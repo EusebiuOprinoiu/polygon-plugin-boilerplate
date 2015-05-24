d9562bfd0abac36287dd833cf67417a3127a3ba3
Eusebiu Oprinoiu <eusebiu.websites@outlook.com>
2015-05-24 05:05:46 +0300

    Require at least PHP Version 5.4
    Require at least PHP Version 5.4.

--- includes/class-polygon-plugin-update-php.php
+++ includes/class-polygon-plugin-update-php.php
@@ -15,7 +15,7 @@ class Polygon_Plugin_Update_PHP {
 
 	// Construct
 	public function __construct() {
-		$this->minimum_version = '5.3';
+		$this->minimum_version = '5.4';
 
 		if ( ! $this->check() ) {
 			add_action( 'admin_notices', array( $this, 'update_php_warning' ) );
