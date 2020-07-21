bf6f6b90abd5e88792cc7f4ec445d1fb9fc9ab3e
Eusebiu Oprinoiu <git@eusebiu.com>
2020-07-21 16:10:18 +0300

    Translation template updated
    Translation template updated.

--- polygon-plugin.php
+++ polygon-plugin.php
@@ -4,7 +4,7 @@
  * Plugin URI:        https://polygonthemes.com/plugins/polygon-plugin
  * Author:            Polygon Themes
  * Author URI:        https://polygonthemes.com
- * Description:       Polygon Plugin Boilerplate is a standardized, organized, object-oriented foundation for building high-quality WordPress plugins. Based on the original work of Tom McFarlin.
+ * Description:       Polygon Plugin Boilerplate is a standardized, organized, object-oriented foundation for building high-quality WordPress plugins.
  * Version:           1.0.0
  * Requires PHP:      7.2
  * Requires at least: 5.0
